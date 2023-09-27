71557df29d268fa283525c59e9af033053d402b5
Ronin1702 <kchen@me.com>
2023-09-27 13:28:26 -0400

    Define the query and mutation functionality to work with the Mongoose models.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,77 @@
+const { User } = require('../models');
+const { GraphQLError } = require('graphql');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    // async getSingleUser({ user = null, params }, res) {/*...*/}
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('savedBooks');
+        
+        return userData;
+      }
+
+      throw new GraphQLError('AUTH_NOT_LOGGED_IN');
+    },
+  },
+  Mutation: {
+    // async login({ body }, res) { /*...*/ }
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new GraphQLError('AUTH_CREDENTIALS_INCORRECT');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new GraphQLError('AUTH_CREDENTIALS_INCORRECT');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    // async createUser({ body }, res) { /*...*/ }
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    // async saveBook({ user, body }, res) { /*...*/ }
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw new GraphQLError('AUTH_NOT_LOGGED_IN');
+    },
+
+    // async deleteBook({ user, params }, res) { /*...*/ }
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+
+        return updatedUser;
+      }
+
+      throw new GraphQLError('AUTH_NOT_LOGGED_IN');
+    },
+  },
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,35 +1,44 @@
+const { gql } = require('graphql-tag');
+
 // Define the GraphQL type definitions
-const typeDefs = `
-type Book {
+const typeDefs = gql`
+  type Book {
     bookId: ID!
     authors: [String!]
     description: String!
     title: String!
     image: String
     link: String
-}
+  }
 
-type User {
+  type User {
     _id: ID!
     username: String!
     email: String!
     savedBooks: [Book]!
     bookCount: Int!
-}
+  }
 
-type Auth {
+  type Auth {
     token: ID!
     user: User!
-}
+  }
 
-type Query {
+  type Query {
     me: User
-}
+  }
 
-type Mutation {
+  type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(authors: [String!], description: String!, title: String!, bookId: ID!, image: String, link: String): User
+    saveBook(
+      authors: [String!]
+      description: String!
+      title: String!
+      bookId: ID!
+      image: String
+      link: String
+    ): User
     removeBook(bookId: ID!): User
   }
 `;
